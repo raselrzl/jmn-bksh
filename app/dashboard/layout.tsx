75f64c915e4d2fa46fa6bd561b778b296e3d42fe
Rasel Miah <rasel6041@gmail.com>
2025-04-01 00:39:36 +0100

    first commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,7 +17,7 @@ import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 import prisma from "@/app/lib/db";
 
-async function getUsers() {
+/* async function getUsers() {
   return await prisma.user.findMany({
     select: {
       id: true,
@@ -29,7 +29,7 @@ async function getUsers() {
     },
   });
 }
-
+ */
 export default async function DashboardLayout({
   children,
 }: {
@@ -38,12 +38,14 @@ export default async function DashboardLayout({
   noStore();
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  // Get users list to check if the current user is admin
-  const users = await getUsers();
+ /*  const users = await getUsers();
   const isAdmin = users.some((u) => u.email === user?.email && u.isAdmin);
   console.log(isAdmin);
   if (!isAdmin) {
-    return redirect("/"); // If isAdmin is false, redirect to home
+    return redirect("/");  */
+  /* } */
+  if (!user || user.email !== "rasel6041@gmail.com") {
+    return redirect("/");
   }
 
   return (
